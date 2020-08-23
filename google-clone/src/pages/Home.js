811edf234f9fc7dadffb4d8bae2d1c7574bea7b0
sarveshwar22 <f20190257@goa.bits-pilani.ac.in>
2020-08-23 05:41:37 +0530

    react router

--- google-clone/src/pages/Home.js
+++ google-clone/src/pages/Home.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import './Home.css'
 
+import HeadsetMicIcon from '@material-ui/icons/HeadsetMic';
+import ImageSearchIcon from '@material-ui/icons/ImageSearch';
+
+
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function Home()
 {
     return (
         <div className='home'>
             <h1>This is home page</h1>
+
+            <div className='home_header'>
+                <div className='home_header_left'>
+                    {/*Link */}
+                    {/*Link */}
+                <div className='home_header_right'>
+                    {/*Link*/}
+                    {/*Link*/}
+                    {/*Icon*/}
+                    {/*Avatar*/}
+                    
+                </div>    
+                </div>
+
+            </div>
+            <div className='home_body'>
+
+            </div>
         </div>
     )
 }
